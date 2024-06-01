830ff0bad1c6a13b33228aa38729cfd892efee89
Gerson <gerson.bermor@gmail.com>
2024-05-31 20:47:29 -0500

    first random

--- lib/pages/dice_pages.dart
+++ lib/pages/dice_pages.dart
@@ -1,6 +1,14 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int n = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +17,7 @@ class DicePage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
               decoration: BoxDecoration(
@@ -20,7 +29,50 @@ class DicePage extends StatelessWidget {
               padding: EdgeInsets.all(16),
               width: 200,
               height: 200,
-              child: Image.asset("assets/images/dice1.png"),
+              child: Image.asset("assets/images/dice$n.png"),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (n == 1) {
+                      n = 6;
+                    } else {
+                      n--;
+                    }
+                    setState(() {});
+                  },
+                  child: Icon(Icons.arrow_back_ios),
+                ),
+                SizedBox(
+                  width: 32,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (n == 6) {
+                      n = 1;
+                    } else {
+                      n++;
+                    }
+                    setState(() {});
+                  },
+                  child: Icon(Icons.arrow_forward_ios),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 8,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                n = Random().nextInt(6) + 1;
+                setState(() {});
+              },
+              child: Text("Random"),
             ),
           ],
         ),
