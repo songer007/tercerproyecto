521ad5f3060d620daa467c8b93ee612aeb7d60c7
Gerson <gerson.bermor@gmail.com>
2024-05-31 20:26:26 -0500

    first radio

--- lib/pages/dice_pages.dart
+++ lib/pages/dice_pages.dart
@@ -5,7 +5,25 @@ class DicePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dice App"),
+        title: Text("Dice Apps"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              padding: EdgeInsets.all(16),
+              width: 200,
+              height: 200,
+              child: Image.asset("assets/images/dice1.png"),
+            ),
+          ],
+        ),
       ),
     );
   }
